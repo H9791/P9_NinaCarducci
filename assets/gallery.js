9a9a414d5262cfb1a0346ba54548d386d23a08f3
JG <johndoe@example.com>
2023-04-18 10:54:29 +0200

    JQuery gallery replaced with JS.

---
+++ assets/gallery.js
@@ -0,0 +1,28 @@
+function ShowCategoryImages(e) {
+    if (e.target.id === "Tous") {
+        document.querySelectorAll('.gallery *').forEach((element, index) => {
+            element.style.display = null;
+        });
+    } else {
+        document.querySelectorAll('.gallery *').forEach((element, index) => {
+            element.style.display = "none";
+        });
+        document.querySelectorAll(`.gallery [category=${e.target.id}]`).forEach((element, index) => {
+            element.style.display = null;
+        });
+    }
+}
+
+function AddCategoryListeners() {
+    document.querySelectorAll("#categories *")
+        .forEach((category, index) => {
+            AddListener(category.id);
+        });
+}
+
+function AddListener(categoryId){
+    document.querySelector(`#categories [id=${categoryId}]`)
+        .addEventListener("click", ShowCategoryImages);
+};
+
+AddCategoryListeners();
